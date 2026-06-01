1c98cb2df36ccc29fe372df84088f301364f9d19
chriscle2013 <chriscle2013@gmail.com>
2026-06-01 02:40:04 -0500

    Refactor entrypoint.sh for JDBC_URL configuration
    Updated DATABASE_URL to JDBC_URL format and added echo statement.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-set -e
-
-# Transforma DATABASE_URL para que tenga el formato jdbc:postgresql://...?sslmode=require
-export DATABASE_URL="jdbc:${DATABASE_URL}?sslmode=require"
-
-# Ejecuta el comando principal
+JDBC_URL="jdbc:postgresql://${DB_HOST}:5432/${DB_NAME}?sslmode=require"
+echo "JDBC_URL is: ${JDBC_URL}"
 exec java -jar app.jar
