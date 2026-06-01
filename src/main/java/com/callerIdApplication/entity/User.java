5ef16a675638ed6cf81f585b7b6db0055b05e8b1
chriscle2013 <chriscle2013@gmail.com>
2026-06-01 11:26:31 -0500

    Implement getters and setters in User class
    Added getters and setters for User entity fields.

--- src/main/java/com/callerIdApplication/entity/User.java
+++ src/main/java/com/callerIdApplication/entity/User.java
@@ -14,28 +14,75 @@ import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import lombok.Data;
-
 @Entity
 @Table(name = "app_user")
 public class User {
 
-		@Id
-		@GeneratedValue(strategy = GenerationType.AUTO)
-	
-		private Integer userId;
-		@NotNull
-		private String userName;
-		@NotNull
-		private String phoneNumber;
-		
-		private String email;
-		
-		@NotNull
-		private String password;
-		
-		@OneToMany(cascade = CascadeType.ALL)
-		@JsonIgnore
-		private List<Contact> contacts=new ArrayList<>();
-		
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer userId;
+    
+    @NotNull
+    private String userName;
+    
+    @NotNull
+    private String phoneNumber;
+    
+    private String email;
+    
+    @NotNull
+    private String password;
+    
+    @OneToMany(cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Contact> contacts = new ArrayList<>();
+
+    // Getters y Setters
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Contact> getContacts() {
+        return contacts;
+    }
+
+    public void setContacts(List<Contact> contacts) {
+        this.contacts = contacts;
+    }
 }
