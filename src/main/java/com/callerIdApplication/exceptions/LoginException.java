f4d2f0c1e7fa097a9d95086472b9d90404a3c19c
chriscle2013 <chriscle2013@gmail.com>
2026-06-01 00:39:08 -0500

    Delete src directory

--- src/main/java/com/callerIdApplication/exceptions/LoginException.java
+++
@@ -1,9 +0,0 @@
-package com.callerIdApplication.exceptions;
-
-public class LoginException extends Exception{
-
-	public LoginException(String msg)
-	{
-		super(msg);
-	}
-}
--- src/main/java/com/callerIdApplication/exceptions/UserException.java
+++
@@ -1,9 +0,0 @@
-package com.callerIdApplication.exceptions;
-
-public class UserException extends Exception{
-
-	public UserException(String msg) {
-		super(msg);
-	}
-	
-}
