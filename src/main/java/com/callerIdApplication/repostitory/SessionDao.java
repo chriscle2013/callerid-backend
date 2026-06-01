5c5bede248d1dba039ccafd0ebf3f3feb2212357
chriscle2013 <chriscle2013@gmail.com>
2026-06-01 13:45:08 -0500

    Add findByUserId method to SessionDao interface

--- src/main/java/com/callerIdApplication/repostitory/SessionDao.java
+++ src/main/java/com/callerIdApplication/repostitory/SessionDao.java
@@ -1,13 +1,16 @@
 package com.callerIdApplication.repostitory;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.callerIdApplication.entity.CurrentUserSession;
 
-
-
+@Repository
 public interface SessionDao extends JpaRepository<CurrentUserSession, Integer> {
-
-	
-	public  CurrentUserSession  findByUuid(String uuid);
+    
+    // Este método ya debería existir (para buscar por UUID)
+    CurrentUserSession findByUuid(String uuid);
+    
+    // 👇 ESTE ES EL MÉTODO NUEVO QUE DEBES AGREGAR (si no existe)
+    CurrentUserSession findByUserId(Integer userId);
 }
