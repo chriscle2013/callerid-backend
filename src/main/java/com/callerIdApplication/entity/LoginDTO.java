d0446f674e1b025fae8a117e3f514f957163cc60
chriscle2013 <chriscle2013@gmail.com>
2026-06-01 14:57:51 -0500

    Enhance LoginDTO with constructors and accessors
    Added constructors, getters, and setters for LoginDTO.

--- src/main/java/com/callerIdApplication/entity/LoginDTO.java
+++ src/main/java/com/callerIdApplication/entity/LoginDTO.java
@@ -1,12 +1,33 @@
 package com.callerIdApplication.entity;
 
-import lombok.Data;
-
-@Data
 public class LoginDTO {
-
-	private String phoneNumber;
-	private String password;
-	
-	
+    
+    private String phoneNumber;
+    private String password;
+    
+    // Constructores
+    public LoginDTO() {
+    }
+    
+    public LoginDTO(String phoneNumber, String password) {
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+    }
+    
+    // Getters y Setters
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+    
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+    
+    public String getPassword() {
+        return password;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
