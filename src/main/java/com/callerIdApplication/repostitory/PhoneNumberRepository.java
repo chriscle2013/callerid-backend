4e705a80200cf46f7b3b5db68ac8957e3c50e326
chriscle2013 <chriscle2013@gmail.com>
2026-06-21 11:28:59 -0500

    Create PhoneNumberRepository interface for phone number queries

---
+++ src/main/java/com/callerIdApplication/repostitory/PhoneNumberRepository.java
@@ -0,0 +1,13 @@
+package com.velo.repository;
+
+import com.velo.model.PhoneNumber;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PhoneNumberRepository extends JpaRepository<PhoneNumber, Long> {
+    // Permite buscar por el número de teléfono
+    Optional<PhoneNumber> findByNumber(String number);
+}
