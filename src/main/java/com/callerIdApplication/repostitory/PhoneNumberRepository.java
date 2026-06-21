faab2d20f16dc05bf7ea875a200f7992e240eae0
chriscle2013 <chriscle2013@gmail.com>
2026-06-21 11:48:36 -0500

    Fix import statement for PhoneNumber model

--- src/main/java/com/callerIdApplication/repostitory/PhoneNumberRepository.java
+++ src/main/java/com/callerIdApplication/repostitory/PhoneNumberRepository.java
@@ -1,6 +1,6 @@
 package com.callerIdApplication.repostitory;
 
-import com.velo.model.PhoneNumber;
+import com.callerIdApplication.model.PhoneNumber;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
